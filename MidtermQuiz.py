1cf4a7eaba7194ad62d8c3f8f84cf0a7e67c1880
James Dayao <123426440+jamesdayao@users.noreply.github.com>
2023-04-04 07:59:15 +0800

    Add files via upload

---
+++ MidtermQuiz.py
@@ -0,0 +1,20 @@
+class DistanceConversion:
+    def __init__ (self, distance):
+        self.__distance = distance
+
+    def distance1(self):
+        return self.__distance * 100
+
+    def distance2(self):
+        return self.__distance / 1000
+
+    def distance3(self):
+        return self.__distance * 39.37
+
+    def display(self):
+        print("Meter to Centimeter: ", self.distance1(), "cm")
+        print("Meter to Kilometer: ", self.distance2(), "km")
+        print("Meter to Inches: ", self.distance3(), "in")
+
+distance = DistanceConversion(float(input("Distance: ")))
+distance.display()
